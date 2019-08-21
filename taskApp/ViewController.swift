1c90c708ad7ec612f083b448af90264e2c7b5130
sena.shimizu <senarugby0813@gmail.com>
2019-08-21 12:20:40 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 清水世那 on 2019/08/21.
+//  Copyright © 2019年 清水世那. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
