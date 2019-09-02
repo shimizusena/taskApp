81ab9966884f15145bbf630853944206f4ab7e2f
sena.shimizu <senarugby0813@gmail.com>
2019-09-02 17:44:38 +0900

    コミット

---
+++ taskApp/InPutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InPutViewController.swift
+//  taskApp
+//
+//  Created by 清水世那 on 2019/08/22.
+//  Copyright © 2019年 清水世那. All rights reserved.
+//
+
+import UIKit
+
+class InPutViewController: UIViewController {
+    @IBOutlet var titleTextField : UITextField!
+    @IBOutlet var comentTextView:UITextView!
+    @IBOutlet var datePickerView:UIPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
